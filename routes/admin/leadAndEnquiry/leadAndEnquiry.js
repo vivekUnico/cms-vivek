11c7b74970335f59693859ead17c36a048210492
vivekKumar <vivek.unicoglobal.com>
2022-12-26 17:27:31 +0530

    done

--- routes/admin/leadAndEnquiry/leadAndEnquiry.js
+++ routes/admin/leadAndEnquiry/leadAndEnquiry.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const { CreateLeadAndEnquiry, GetAllLeadAndEnquiry, GetSingleLeadAndEnquiry, DeleteLeadAndEnquiry, UpdateLead, 
-        UpdateEnquiry, MoveLeadToEnquiry } = require("../../../controllers/leadAndEnquiry");
+        GetLeadAndEnquiryByFilter,  UpdateEnquiry, MoveLeadToEnquiry } = require("../../../controllers/leadAndEnquiry");
 
 router.route('/').get(GetAllLeadAndEnquiry)
 router.route('/').post(CreateLeadAndEnquiry)
+router.route('/filter').get(GetLeadAndEnquiryByFilter);
 
 router.route('/:id').get(GetSingleLeadAndEnquiry)
 router.route('/:id').delete(DeleteLeadAndEnquiry)
