4aca23be37836547236d31253ed5579e9541b310
vivekKumar <vivek.unicoglobal.com>
2022-12-27 16:05:09 +0530

    done

--- routes/admin/followup.js
+++ routes/admin/followup.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { GetAllFollowup, GetSingleFollowup, CreateFollowup, DeleteSingleFollowup } = require("../../controllers/followup");
+const { GetAllFollowup, GetSingleFollowup, GetFollowupByFilter, UpdateSingleFollowup, newDeleteSingleFollowup,
+    CreateFollowup, DeleteSingleFollowup } = require("../../controllers/followup");
 
 router.route('/').get(GetAllFollowup)
 router.route('/').post(CreateFollowup).put(CreateFollowup)
+
+router.route('/filter').get(GetFollowupByFilter)
+router.route('/:connection_id/:addedTime').put(UpdateSingleFollowup).delete(newDeleteSingleFollowup)
 
 router.route('/:connection_id').get(GetSingleFollowup)
 router.route('/:connection_id').delete(DeleteSingleFollowup)
