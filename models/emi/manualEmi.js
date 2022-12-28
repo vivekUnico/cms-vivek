724ff58e22e018f22e3fbbbeff03c073fd8fe5c3
salimUnico <salim.unicoglobal@gmail.com>
2022-12-29 02:27:34 +0530

    salim asdaswqed

--- models/emi/manualEmi.js
+++ models/emi/manualEmi.js
@@ -9,27 +9,27 @@ const manualEmiScheme = new mongoose.Schema({
         type: Date,
         default: new Date(),
     },
-    NIPay : Number,
-    BanckName:  String,
-    cheque : string,
-    city : string,
-    date : Date,
-    mode : {
+    NIPay: Number,
+    BanckName: String,
+    cheque: String,
+    city: String,
+    date: Date,
+    mode: {
         type: String,
-        enum : ["Cash", "Cheque", "Online Gateway", "Demand Draft", 
-            "Card Swipe", "Internet Banking", "Paytm", "Cash Deposit", 
+        enum: ["Cash", "Cheque", "Online Gateway", "Demand Draft",
+            "Card Swipe", "Internet Banking", "Paytm", "Cash Deposit",
             "Google Pay", "Phone Pay", "PayU Money", "Amazon Pay"],
     },
-    modeDate : Date,
-    paid : {
-        type : Number,
+    modeDate: Date,
+    paid: {
+        type: Number,
         default: 0,
     },
-    recepit : string,
-    remark : string,
-    tax : {
-        type : string,
-        enum : ['GST', 'IGST', 'CGST', 'SGST'],
+    recepit: String,
+    remark: String,
+    tax: {
+        type: String,
+        enum: ['GST', 'IGST', 'CGST', 'SGST'],
     },
 });
 
