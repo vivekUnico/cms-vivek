83b29dd1c668c250076773b9ff725316761f8c1b
vivekKumar <vivek.unicoglobal@gmail.com>
2023-03-23 19:46:46 +0530

    temp

--- models/leadAndEnquiry/index.js
+++ models/leadAndEnquiry/index.js
@@ -23,7 +23,7 @@ let mainSchema = {
             /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             "Please provide a valid email address",
         ],
-        // unique: true,
+        unique: true,
     },
     date: {
         type: Date,
